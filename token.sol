bd9a024251f8ae1652f1953f67d1b491fa2b0775
OCORUJA <rcazelli@yahoo.com.br>
2024-08-18 11:23:27 -0300

    feat add OCORUJA profile

--- token.sol
+++ token.sol
@@ -1,70 +1,114 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.7;
-
+pragma solidity ^0.4.24;
+ 
+//Safe Math Interface
+ 
+contract SafeMath {
+ 
+    function safeAdd(uint a, uint b) public pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
+    }
+ 
+    function safeSub(uint a, uint b) public pure returns (uint c) {
+        require(b <= a);
+        c = a - b;
+    }
+ 
+    function safeMul(uint a, uint b) public pure returns (uint c) {
+        c = a * b;
+        require(a == 0 || c / a == b);
+    }
+ 
+    function safeDiv(uint a, uint b) public pure returns (uint c) {
+        require(b > 0);
+        c = a / b;
+    }
+}
+ 
+ 
 //ERC Token Standard #20 Interface
-interface ERC20Interface{
-    function totalSupply() external view returns (uint);
-    function balanceOf(address tokenOwner) external view returns (uint balance);
-    function allowance(address tokenOwner, address spender) external view returns (uint remaining);
-
-    function transfer(address to, uint tokens) external returns (bool success);
-    function approve(address spender, uint tokens) external returns (bool success);
-    function transferFrom(address from, address to, uint tokens) external returns (bool success);
+ 
+contract ERC20Interface {
+    function totalSupply() public constant returns (uint);
+    function balanceOf(address tokenOwner) public constant returns (uint balance);
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
  
     event Transfer(address indexed from, address indexed to, uint tokens);
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
  
-//Actual token contract 
-contract DIOToken is ERC20Interface{
-    string public symbol = "DIO" ;
-    string public  name = "DIO Coin";
-    uint8 public decimals = 2;
-    uint256 public _totalSupply;
+ 
+//Contract function to receive approval and execute function in one call
+ 
+contract ApproveAndCallFallBack {
+    function receiveApproval(address from, uint256 tokens, address token, bytes data) public;
+}
+ 
+//Actual token contract
+ 
+contract DIOToken is ERC20Interface, SafeMath {
+    string public symbol;
+    string public  name;
+    uint8 public decimals;
+    uint public _totalSupply;
  
     mapping(address => uint) balances;
     mapping(address => mapping(address => uint)) allowed;
  
-    constructor() {
-        _totalSupply = 1000000;
-        balances[msg.sender] = _totalSupply;
+    constructor() public {
+        symbol = "PDI";
+        name = "Project DIO Coin";
+        decimals = 2;
+        _totalSupply = 100000;
+        balances[0xd054EBff98Fff91c4E3A5aA4e4363fA45Bd94671] = _totalSupply;
+        emit Transfer(address(0), 0xd054EBff98Fff91c4E3A5aA4e4363fA45Bd94671
+        , _totalSupply);
     }
  
-    function totalSupply() public override view returns (uint256) {
-        return _totalSupply;
+    function totalSupply() public constant returns (uint) {
+        return _totalSupply  - balances[address(0)];
     }
  
-    function balanceOf(address tokenOwner) public override view returns (uint256) {
+    function balanceOf(address tokenOwner) public constant returns (uint balance) {
         return balances[tokenOwner];
     }
  
-    function transfer(address to, uint tokens) public override returns (bool success) {
-        require(tokens <= balances[msg.sender]);
-
-        balances[msg.sender] = balances[msg.sender] - tokens;
-        balances[to] = balances[to] + tokens;
+    function transfer(address to, uint tokens) public returns (bool success) {
+        balances[msg.sender] = safeSub(balances[msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
         emit Transfer(msg.sender, to, tokens);
         return true;
     }
  
-    function approve(address spender, uint256 tokens) public override returns (bool success) {
+    function approve(address spender, uint tokens) public returns (bool success) {
         allowed[msg.sender][spender] = tokens;
         emit Approval(msg.sender, spender, tokens);
         return true;
     }
-
-    function allowance(address tokenOwner, address spender) public override view returns (uint remaining) {
-        return allowed[tokenOwner][spender];
-    }
-
-    function transferFrom(address from, address to, uint256 tokens) public override returns (bool success) {
-        require(tokens <= balances[msg.sender]);
-        require(tokens <= allowed[from][msg.sender]);
-
-        balances[from] = balances[from] - tokens;
-        allowed[from][msg.sender] = allowed[from][msg.sender] - tokens;
-        balances[to] = balances[to] + tokens;
+ 
+    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+        balances[from] = safeSub(balances[from], tokens);
+        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
         emit Transfer(from, to, tokens);
         return true;
+    }
+ 
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
+        return allowed[tokenOwner][spender];
+    }
+ 
+    function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
+        return true;
+    }
+ 
+    function () public payable {
+        revert();
     }
 } 
